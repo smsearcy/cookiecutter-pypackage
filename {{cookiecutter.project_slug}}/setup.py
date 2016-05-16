2055c014cf9470bf8628412bdee4f69c1620f0d8
Scott Searcy <ssearcy@roguefcu.internal>
2016-05-15 23:44:34 -0700

    Switched from `codecs.open()` to `io.open` and used simplified `long_description` from webpage instead of `attrs` project. Changed quotes to single quotes because that's what I prefer.

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copied from https://github.com/hynek/attrs/blob/master/setup.py
-# See https://hynek.me/articles/sharing-your-labor-of-love-pypi-quick-and-dirty/
-# for more information.
+# Copied from https://hynek.me/articles/sharing-your-labor-of-love-pypi-quick-and-dirty/
 
-import codecs
+import io
 import os
 import re
 
@@ -14,23 +12,23 @@ from setuptools import setup, find_packages
 
 ###############################################################################
 
-NAME = "{{ cookiecutter.project_slug }}"
-PACKAGES = find_packages(where="src")
-META_PATH = os.path.join("src", "{{ cookiecutter.project_slug }}", "__init__.py")
+NAME = '{{ cookiecutter.project_slug }}'
+PACKAGES = find_packages(where='src')
+META_PATH = os.path.join('src', '{{ cookiecutter.project_slug }}', '__init__.py')
 KEYWORDS = []
 CLASSIFIERS = [
     'Development Status :: 2 - Pre-Alpha',
     'Intended Audience :: Developers',
-    "Natural Language :: English",
-    "License :: OSI Approved :: MIT License",
-    "Operating System :: OS Independent",
-    "Programming Language :: Python",
-    "Programming Language :: Python :: 2",
-    "Programming Language :: Python :: 2.7",
-    "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.4",
-    "Programming Language :: Python :: 3.5",
-    "Programming Language :: Python :: Implementation :: CPython",
+    'Natural Language :: English',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: Implementation :: CPython',
 ]
 INSTALL_REQUIRES = []
 
@@ -44,7 +42,7 @@ def read(*parts):
     Build an absolute path from *parts* and and return the contents of the
     resulting file.  Assume UTF-8 encoding.
     """
-    with codecs.open(os.path.join(HERE, *parts), "rb", "utf-8") as f:
+    with io.open(os.path.join(HERE, *parts), 'r', encoding='utf-8') as f:
         return f.read()
 
 
@@ -56,42 +54,29 @@ def find_meta(meta):
     Extract __*meta*__ from META_FILE.
     """
     meta_match = re.search(
-        r"^__{meta}__ = ['\"]([^'\"]*)['\"]".format(meta=meta),
+        r'^__{meta}__ = ["\']([^"\']*)["\']'.format(meta=meta),
         META_FILE, re.M
     )
     if meta_match:
         return meta_match.group(1)
-    raise RuntimeError("Unable to find __{meta}__ string.".format(meta=meta))
+    raise RuntimeError('Unable to find __{meta}__ string.'.format(meta=meta))
 
 
-URI = find_meta("uri")
-LONG = (
-    read("README.rst") + "\n\n" +
-    "Release Information\n" +
-    "===================\n\n" +
-    re.search("(\d+.\d.\d \(.*?\)\n.*?)\n\n\n----\n\n\n",
-              read("CHANGELOG.rst"), re.S).group(1) +
-    "\n\n`Full changelog " +
-    "<{uri}en/stable/changelog.html>`_.\n\n".format(uri=URI) +
-    read("AUTHORS.rst")
-)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     setup(
         name=NAME,
-        description=find_meta("description"),
-        license=find_meta("license"),
-        url=URI,
-        version=find_meta("version"),
-        author=find_meta("author"),
-        author_email=find_meta("email"),
-        maintainer=find_meta("author"),
-        maintainer_email=find_meta("email"),
+        description=find_meta('description'),
+        license=find_meta('license'),
+        url=find_meta('uri'),
+        version=find_meta('version'),
+        author=find_meta('author'),
+        author_email=find_meta('email'),
+        maintainer=find_meta('author'),
+        maintainer_email=find_meta('email'),
         keywords=KEYWORDS,
-        long_description=LONG,
+        long_description=read('README.rst'),
         packages=PACKAGES,
-        package_dir={"": "src"},
+        package_dir={'': 'src'},
         zip_safe=False,
         classifiers=CLASSIFIERS,
         install_requires=INSTALL_REQUIRES,
